8d7e91c2d026c9fe8b07cdcc4586876f5f6a45f2
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-01-03 14:38:06 +0530

    post

--- 04primitivesobjects/index.js
+++ 04primitivesobjects/index.js
@@ -5,15 +5,15 @@ let c = true; // can also be false
 let d = BigInt("567") + BigInt("3")
 let e = "Aman"
 let f = Symbol("I am a nice symbol")
-let g
-console.log(a, b, c , d, e, f, g)
-console.log(typeof c)
+let g 
+// console.log(a, b, c , d, e, f, g)
+console.log(typeof g)
 
 // Non Primitive Data Type - Objects in Js
 const item = {
-  "Aman": true,
+  "Aman": true, 
   "Shubh": false,
   "Lovish": 67,
   "Rohan": undefined
 }
-console.log(item["Shubh"])
+console.log(item.Shubh)
