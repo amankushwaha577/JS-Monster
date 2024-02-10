89d7d8db58eed8e66dd5cb246e831468afec47ab
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-02-10 17:22:08 +0530

    map

--- 106.polyfill_call_apply_bind/index.js
+++ 106.polyfill_call_apply_bind/index.js
@@ -1,83 +1,85 @@
-/******** MAP =  START WRITING CODE HERE *************/
+function greet(greeting) {
+  console.log(`${greeting}, ${this.name}!`);
+}
 
-Array.prototype.mymap = function (callback, thisArg) {
-    if (typeof callback !== 'function') {
-      throw new TypeError('Callback must be a function');
-    }
-  
-    const array = this;
-    const length = array.length;
-    const mappedArray = new Array(length);
-  
-    for (let i = 0; i < length; i++) {
-        mappedArray[i] = callback.call(thisArg, array[i], i, array);
-    }
-  
-    return mappedArray;
+const person = {
+  name: 'John'
+};
+
+/****************** CALL() = START WRITING CODE HERE *************************/
+Function.prototype.myCall = function(context, ...args) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Function.prototype.myCall - the object is not callable');
+  }
+
+  // If context is not provided, default to the global object (window in a browser)
+  context = context || globalThis;
+
+  // Create a unique key to avoid overwriting properties on the context object
+  const uniqueKey = Symbol();
+  context[uniqueKey] = this;
+
+  // Call the function with the modified context and arguments
+  const result = context[uniqueKey](...args);
+
+  // Remove the temporary property from the context object
+  delete context[uniqueKey];
+
+  return result;
+
+};
+greet.myCall(person, 'Hello');
+/****************** CODE EDITING BLOCK ENDS HERE *********************/
+
+
+
+/****************** START WRITING CODE HERE *************************/
+Function.prototype.myApply = function(context, argsArray) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Function.prototype.myApply - the object is not callable');
+  }
+
+  // If context is not provided, default to the global object (window in a browser)
+  context = context || globalThis;
+
+  // Ensure argsArray is an array or array-like object
+  argsArray = Array.isArray(argsArray) ? argsArray : Array.from(argsArray);
+
+  // Create a unique key to avoid overwriting properties on the context object
+  const uniqueKey = Symbol();
+  context[uniqueKey] = this;
+
+  // Call the function with the modified context and arguments array
+  const result = context[uniqueKey](...argsArray);
+
+  // Clean up by removing the temporary property from the context object
+  delete context[uniqueKey];
+
+  return result;
+};
+greet.myApply(person, ['Hello']);
+/****************** CODE EDITING BLOCK ENDS HERE *********************/
+
+
+/****************** START WRITING CODE HERE *************************/
+Function.prototype.myBind = function(context, ...args) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Function.prototype.myBind - the object is not callable');
+  }
+
+  // Save a reference to the current function
+  const originalFunction = this;
+
+  // Return a new function that, when called, will apply the original function with the specified context and arguments
+  return function(...innerArgs) {
+    // Apply the original function with the context and concatenated arguments
+    return originalFunction.apply(context, args.concat(innerArgs));
   };
-
-  let arr = [1,2,3];
-  arr= arr.mymap((element)=>element+1);
-  console.log(arr);
-  
-/****************** CODE EDITING BLOCK ENDS HERE *************/
-
-
-/******** FILTER =  START WRITING CODE HERE *************/
-Array.prototype.myfilter = function(callback, thisArg) {
-    if (typeof callback !== 'function') {
-      throw new TypeError('Callback must be a function');
-    }
-  
-    const array = this;
-    const length = array.length;
-    const filteredArray = [];
-  
-    for (let i = 0; i < length; i++) {
-        if (callback.call(thisArg, array[i], i, array)) {
-          filteredArray.push(array[i]);
-        }
-    }
-  
-    return filteredArray;
-  };
-
-  const numbers = [1, 2, 3, 4, 5];
-  const evenNumbers = numbers.myfilter(num => num % 2 === 0);
-  console.log(evenNumbers); // Output: [2, 4]
-
-/****************** CODE EDITING BLOCK ENDS HERE *************/
+};
+// Bind the greet function to the person object
+const boundGreet = greet.myBind(person, 'Hello');
+boundGreet('!'); // Output: Hello, Alice!
+/****************** CODE EDITING BLOCK ENDS HERE *********************/
 
 
-/******** REDUCE =  START WRITING CODE HERE *************/
-Array.prototype.myreduce = function (callback, initialValue) {
-    if (typeof callback !== 'function') {
-      throw new TypeError('Callback must be a function');
-    }
-  
-    const array = this;
-    const length = array.length;
-  
-    // Check if the array is empty and no initial value is provided
-    if (length === 0 && initialValue === undefined) {
-      throw new TypeError('Reduce of empty array with no initial value');
-    }
-  
-    let accumulator = initialValue !== undefined ? initialValue : array[0];
-    let startIndex = initialValue !== undefined ? 0 : 1;
-  
-    for (let i = startIndex; i < length; i++) {
-        accumulator = callback(accumulator, array[i], i, array);
-      }
-  
-    return accumulator;
-  };
 
-  const numbers2 = [1, 2, 3, 4, 5];
-  // Example usage
-  const sum = numbers2.myreduce((acc, num) => acc + num, 0);
-  console.log(sum); // Output: 15
-
-  
-  /****************** CODE EDITING BLOCK ENDS HERE *********************/
-  
